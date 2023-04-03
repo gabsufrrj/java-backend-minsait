c8a7bcc71613fbfa026ad7aedbf168a139ff2130
gabsgregorio <gabsufrrj@gmail.com>
2023-04-03 12:26:25 -0300

    feat: java main application and test

---
+++ loan-api/src/main/java/com/minsait/loanapi/LoanApiApplication.java
@@ -0,0 +1,13 @@
+package com.minsait.loanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanApiApplication.class, args);
+	}
+
+}
---
+++ loan-api/src/test/java/com/minsait/loanapi/LoanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minsait.loanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
