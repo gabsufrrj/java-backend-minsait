c9ff83c8255e8dc7e19c14da947cc4a5e7398c4b
gabsgregorio <gabsufrrj@gmail.com>
2023-04-04 20:02:24 -0300

    feat: implements the enum to calculate value to customers Ouro

--- loan-api/src/main/java/com/minsait/loanapi/types/Relacionamento.java
+++ loan-api/src/main/java/com/minsait/loanapi/types/Relacionamento.java
@@ -3,11 +3,12 @@ package com.minsait.loanapi.types;
 import java.math.BigDecimal;
 
 
+
 public enum Relacionamento {
 	
 	Bronze(1) {
 		@Override
-		public BigDecimal calculaValorFinal (BigDecimal valorInicial) {
+		public BigDecimal calculaValorFinal (BigDecimal valorInicial, int numEmprestimos) {
 			BigDecimal multiplicador = new BigDecimal("1.80");
 			return valorInicial.multiply(multiplicador);
 		}
@@ -15,7 +16,7 @@ public enum Relacionamento {
 	
 	Prata(2) {
 		@Override
-		public BigDecimal calculaValorFinal (BigDecimal valorInicial) {
+		public BigDecimal calculaValorFinal (BigDecimal valorInicial, int numEmprestimos) {
 			BigDecimal valueToCompare = new BigDecimal("5000");
 			if (valorInicial.compareTo(valueToCompare) == 1) {
 				BigDecimal multiplicador = new BigDecimal("1.40");
@@ -28,36 +29,36 @@ public enum Relacionamento {
 
 			}
 		}
+	},
+	
+	Ouro(3) {
+		@Override
+		public BigDecimal calculaValorFinal (BigDecimal valorInicial, int numEmprestimos) {
+			
+			if (numEmprestimos > 1 && numEmprestimos != 0 ) {
+				BigDecimal multiplicador = new BigDecimal("1.3");
+				return valorInicial.multiply(multiplicador);
+			}
+			
+			else {
+				BigDecimal multiplicador = new BigDecimal("1.2");
+				return valorInicial.multiply(multiplicador);
+			}
+			
+		}
 	};
 	
-//	Ouro(3) {
-//		@Override
-//		public BigDecimal calculaValorFinal (BigDecimal valorInicial) {
-//			
-//			
-////			if () {
-////				BigDecimal multiplicador = new BigDecimal("1.2");
-////				return valorInicial.multiply(multiplicador);
-////			} 
-////			
-////			else {
-////				BigDecimal multiplicador = new BigDecimal("1.60");
-////				return valorInicial.multiply(multiplicador);
-////
-////			}
-//		}
-//	};
 	
+	private int codigo;
+
 	private Relacionamento(int codigo) {
 		this.codigo = codigo;
-	}
-
-	private int codigo;
-	
+	}	
 	
 	public int getCodigo() {
 		return this.codigo;
 	}
 	
-	public abstract BigDecimal calculaValorFinal(BigDecimal valorInicial);
+
+	public abstract BigDecimal calculaValorFinal(BigDecimal valorInicial, int numEmprestimos);
 }
