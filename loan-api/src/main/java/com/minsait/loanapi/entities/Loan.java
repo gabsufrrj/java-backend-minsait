aff123f0e3d92433f9ae5f30ccfa452cd759dd23
gabsgregorio <gabsufrrj@gmail.com>
2023-04-10 16:09:55 -0300

    chore: remove unused imports

--- loan-api/src/main/java/com/minsait/loanapi/entities/Loan.java
+++ loan-api/src/main/java/com/minsait/loanapi/entities/Loan.java
@@ -1,14 +1,13 @@
 package com.minsait.loanapi.entities;
 
 import java.math.BigDecimal;
-import java.security.Timestamp;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.Range;
